6ec7b81bcc8119a10e2bfa5ca88d0a41267ee794
icadir <ismailcadir@gmail.com>
2019-11-25 00:36:59 +0300

    Product List

--- CoreProjects/Core.EntityFrameWork/Controllers/ProductController.cs
+++ CoreProjects/Core.EntityFrameWork/Controllers/ProductController.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.EntityFrameWork.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core.EntityFrameWork.Controllers
 {
     public class ProductController : Controller
     {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult List() => View(repository.Products);
     }
 }
--- CoreProjects/Core.EntityFrameWork/Startup.cs
+++ CoreProjects/Core.EntityFrameWork/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.EntityFrameWork.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -23,6 +24,7 @@ namespace Core.EntityFrameWork
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddMvc();
         }
 
