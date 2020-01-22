f48005114e785423f65dc0202600960f763a4934
icadir <ismailcadir@gmail.com>
2020-01-22 22:41:53 +0300

    Blog And Category List

---
+++ CoreProjects/BlogApp.WebUI/Controllers/BlogController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApp.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.WebUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private IBlogRepository repository;
+
+        public BlogController(IBlogRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(repository.GetAll());
+        }
+    }
+}
---
+++ CoreProjects/BlogApp.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApp.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICategoryRepository repository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            this.repository = categoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(repository.GetAll());
+
+        }
+    }
+}
