6647d0e78e9ac7c183f56667ee0b60c2e40f3693
icadir <ismailcadir@gmail.com>
2020-01-25 18:40:19 +0300

    Adding Cateory Components

---
+++ CoreProjects/BlogApp.WebUI/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApp.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.WebUI.ViewComponents
+{
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private ICategoryRepository _repository;
+
+        public CategoryMenuViewComponent(ICategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_repository.GetAll());
+        }
+    }
+}
